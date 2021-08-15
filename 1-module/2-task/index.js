a38b7f5773df203cf57dad3de4a65cb1748070c4
Curly Otter <khruschevdmitriy@gmail.com>
2021-08-15 16:14:27 +0300

    task 2

--- 1-module/2-task/index.js
+++ 1-module/2-task/index.js
@@ -10,8 +10,7 @@ function print(text) {
  * чтобы функция sayHello работала корректно
  */
 function isValid(name) {
-  return name && name.length > 3 && 
-  name.length === name.trim().length
+  return name && name.length > 3 && name.indexOf(' ') < 1
 }
 
 function sayHello() {
