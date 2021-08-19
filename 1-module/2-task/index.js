28f1253f9558163cf5593cdadb0897e1364f91eb
Curly Otter <khruschevdmitriy@gmail.com>
2021-08-19 13:16:55 +0300

    2-module-task-1

--- 1-module/2-task/index.js
+++ 1-module/2-task/index.js
@@ -10,7 +10,7 @@ function print(text) {
  * чтобы функция sayHello работала корректно
  */
 function isValid(name) {
-  return name && name.length > 3 && name.indexOf(' ') < 1
+  return !!name && !name.includes(' ') && name.length > 3 
 }
 
 function sayHello() {
