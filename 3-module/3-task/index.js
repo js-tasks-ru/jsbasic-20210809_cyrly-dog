a92815c9d0f3e60c4767978acd3cd3973fe04e9c
Curly Otter <khruschevdmitriy@gmail.com>
2021-08-24 14:29:19 +0300

    3 module

--- 3-module/3-task/index.js
+++ 3-module/3-task/index.js
@@ -1,3 +1,11 @@
 function camelize(str) {
-  // ваш код...
+  const lettersArr = str.split("");
+  lettersArr.forEach((letter, index) => { 
+    lettersArr[index] = letter
+    if (letter === "-") {
+      lettersArr[index + 1] = lettersArr[index +1].toUpperCase()
+    }
+  })
+  
+  return result = lettersArr.join('').split("-").join('')
 }
--- 3-module/5-task/index.js
+++ 3-module/5-task/index.js
@@ -1,3 +1,14 @@
 function getMinMax(str) {
-  // ваш код...
+  let numbers = []
+  
+  str.split(" ").forEach((item, index) => {
+   if (isFinite(parseFloat(item))) {
+     numbers.push(parseFloat(item))
+   }
+  })
+  
+  return result = {
+    min: Math.min(...numbers),
+    max: Math.max(...numbers)
+  }
 }
