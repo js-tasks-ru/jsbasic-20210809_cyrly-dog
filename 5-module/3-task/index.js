0f98562d8d6a080abd813e381aba33c7fa0e63ec
Curly Otter <khruschevdmitriy@gmail.com>
2021-09-05 18:58:27 +0300

    5-module

--- 5-module/3-task/index.js
+++ 5-module/3-task/index.js
@@ -1,3 +1,36 @@
 function initCarousel() {
-  // ваш код...
+  const left = document.querySelector(".carousel__arrow_left");
+  const right = document.querySelector(".carousel__arrow_right");
+  const slider = document.querySelector(".carousel__inner");
+  const sliderWidth = slider.offsetWidth;
+  let moveWidth = 0
+
+  left.style.display = "none";
+
+  let count = 1;
+
+  right.addEventListener("click", () => {
+    if (count < 4) {
+      moveWidth -= sliderWidth
+      slider.style.transform = `translateX(${moveWidth}px)`
+      count++
+      if (count === 4) {
+        right.style.display = 'none'
+      } 
+      left.style.display = ''
+    }
+  })
+
+  left.addEventListener("click", () => {
+    if (count > 1) {
+      moveWidth += sliderWidth
+      slider.style.transform = `translateX(${moveWidth}px)`
+      count--
+      if (count === 1) {
+        left.style.display = 'none'
+      }
+      right.style.display = ''
+    }
+  })
+
 }
